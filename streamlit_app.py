1cbabe95f1fd563b541a720235c2a40b4d0835e6
Mani <sivamani.patluri@gmail.com>
2022-07-27 12:23:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,5 @@ my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains")
 streamlit.dataframe(my_data_row)
+streamlit.header("What fruit would you like to add")
+
